e6b784f1627373c4f8c5259d95607c17e0f9227f
Hoa <nguyenhuynhnhuhoa1405@smth.smth>
2025-12-04 13:00:58 +0700

    Update

--- Selenium/Ex04.py
+++ Selenium/Ex04.py
@@ -11,11 +11,10 @@ import getpass
 my_username = input('Please provide your username:')
 my_password = getpass.getpass('Please provide your password:')
 
-# Cấu hình để trình duyệt không tự tắt ngay lập tức nếu lỗi
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True) 
 
-# Tắt thông báo "Chrome is being controlled by automated software" để đỡ bị Reddit soi
+# Tắt thông báo "Chrome is being controlled by automated software" 
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 
@@ -24,15 +23,13 @@ driver.maximize_window()
 
 print("--- BẮT ĐẦU ---")
 
-# try:
+
 # 1. MỞ TRANG ĐĂNG NHẬP
 url = 'https://www.reddit.com/login/'
 driver.get(url)
 print("Đang tải trang login...")
 # ------
 print("----------------------------------------------------")
-print("HÃY DÙNG TAY ĐỂ XÁC MINH ROBOT VÀ ĐĂNG NHẬP TRÊN TRÌNH DUYỆT")
-print("Sau khi bạn đã đăng nhập thành công và vào trang chủ Reddit...")
 input("HÃY BẤM PHÍM ENTER TẠI ĐÂY ĐỂ TIẾP TỤC CHẠY TOOL...") 
 print("----------------------------------------------------")
 # ---------------------
@@ -56,43 +53,40 @@ print("Đã điền Pass.")
 password_input.send_keys(Keys.ENTER)
 print("Đã nhấn Enter. Đang chờ chuyển trang...")
     
-    # Chờ 10s để xem có vào được không
 time.sleep(10)
-   # --- PHẦN ĐĂNG BÀI ---
-try:
-    driver.switch_to.default_content()
 
-    url2 = 'https://www.reddit.com/user/chloechooha/submit/?type=TEXT'
-    driver.get(url2)
-    print("4. Đang vào trang đăng bài...")
 
-    # Chờ ô nhập Title
-    title_box = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Title*']"))
-    )
-    title_box.send_keys("Test Post via Selenium")
-    print("Đã nhập tiêu đề.")
+# Truy cap trang post bai
+url2 = 'https://www.reddit.com/user/chloechooha/submit/?type=TEXT'
+driver.get(url2);
+time.sleep(2)
+# Nhập Title
+print("Đang tìm ô nhập ")
+# Tìm ô Title
+title_box = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.NAME, "title"))
+)
+title_box.send_keys('Test')
+print("Đã điền nội dung.")
 
-    # Chờ ô nhập body
-    body_box = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@role='textbox']"))
-    )
-    body_box.click()
-    body_box.send_keys("This is the content of the automated Selenium post.")
-    print("Đã nhập nội dung.")
+# Nhập Body
+print("Đang tìm ô nhập ")
+body_box = driver.find_element(By.NAME,"body") 
 
-    # Chờ nút Post hoạt động
-    post_button = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[.='Post']"))
-    )
-    post_button.click()
-    print("Đã bấm Post!")
+body_box.send_keys('Nguyen Huynh Nhu Hoa') 
+print("Đã điền nội dung.")
 
-except Exception as e:
-    print("\n[LỖI]:", e)
+# Lấy shadow root của phần submit
+shadow_host = driver.find_element(By.CSS_SELECTOR, "r-post-form-submit-button#submit-post-button")
+shadow_root = shadow_host.shadow_root
+# Trong shadow-root có 1 <button>
+post_button = shadow_root.find_element(By.CSS_SELECTOR, "button")
 
-finally:
-    print("Đợi 120s để xem kết quả rồi thoát...")
-    time.sleep(120)
-    driver.quit()
 
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(post_button))
+
+post_button.click()
+print(">>> POST THÀNH CÔNG!")
+
+time.sleep(15)
+driver.quit()
